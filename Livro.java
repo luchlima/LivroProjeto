5f28294b64d0f8e3a8531c102ffca4b14b08d55e
Lucas_Hanz <92007053+luchlima@users.noreply.github.com>
2024-06-09 13:43:58 -0300

    Initial commit

---
+++ Livro.java
@@ -0,0 +1,30 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+
+    public Livro (String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public void exibirDetalhes() {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Livro livro = new Livro("TWD", "Lucas");
+
+        livro.exibirDetalhes();
+    }
+}
